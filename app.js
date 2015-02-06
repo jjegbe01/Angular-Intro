a1903c33616481445585553ea3387a11df1123ad
Nick Wolf <njwolf64@gmail.com>
2015-02-05 20:55:21 -0500

    fix  app ls

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var app = angular.module('store',[]);
+(function() {
+  var app = angular.module('gemStore', ['store-directives']);
+
+  app.controller('StoreController', ['$http', function($http){
+  var store = this;
+  store.products = [];
+
+  $http.get('/store-products.json').success(function(data){
+    store.products = data;
+  });
+}]);
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
+})();
